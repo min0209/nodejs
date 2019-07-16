3b357179c8e966204c2b370d81490cde201f5ad4
Imhunwoo <39878023+sera9786@users.noreply.github.com>
2019-07-16 23:03:49 +0900

    Update callback.js

--- example/callback.js
+++ example/callback.js
@@ -1,14 +1,57 @@
-function a(calllback){
-    const whoami = "i am A"
-    calllback(whoami)
-  }
+function callback1(){
+    function a(calllback){
+        const whoami = "i am A"
+        calllback(whoami)
+    }
+    
+    function b(str){
+        console.log("받은값 : ",str); 
+    }
+    
+    a(function(str){
+        console.log("받은값 : ", str)
+    })
+    
+    a(b)
+}
+
+function callback2(){
+    function step1(callback){
+        setTimeout(function(){
+            console.log("step1 run....");
+        }, 2000)
+    }
   
-  function b(str){
-    console.log("받은값 : ",str);
-  }
-  
-  a(function(str){
-    console.log("받은값 : ", str)
-  })
-  
-  a(b)
+    function step2(){
+        setTimeout(function(){
+            console.log("step2 run....");
+        }, 1000)
+    }
+    step1();
+    step2();
+}
+
+function callback3(){
+    function step1(callback){
+        setTimeout(function(){
+            console.log("step1 run....");
+            callback()
+        },2000)
+    }
+
+    function step2(callback){
+        setTimeout(function(){
+            console.log("step2 run....");
+        },1000)
+    }
+
+    step1(step2); // 표현방법 1
+
+    // step1(function(){ // 표현방법 2
+    //     setTimeout(function(){
+    //         console.log("step2 run....");
+    //     },1000)
+    // })s
+}
+
+callback3();
