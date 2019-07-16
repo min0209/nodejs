207ab397102e391bb3d58e2aad75b0116b8f6129
Imhunwoo <39878023+sera9786@users.noreply.github.com>
2019-07-16 23:33:58 +0900

    promise

--- example/promise.js
+++ example/promise.js
@@ -1,26 +1,52 @@
 //https://medium.com/@appear.ko/javascript-async-to-sync-157c57208598
-function foo(){
-    return new Promise(function(resolve, reject){
-        try{
-            resolve("i am Promise")
-        } catch(error){
-            reject(error)
-        }
+function promise1(){
+    function foo(){
+        return new Promise(function(resolve, reject){
+            try{
+                resolve("i am Promise")
+            } catch(error){
+                reject(error)
+            }
+        })
+    }
+    
+    foo().then(function(args){
+        console.log(args)
     })
 }
 
-foo().then(function(args){
-    console.log(args)
-})
 
+function promise2(){
+    var promise = new Promise(function(resolve, reject) {
+        resolve(1);
+    });
+    
+    promise.then(function(val){
+        console.log(val);
+        return val + 2;
+    }).then(function(val){
+        console.log(val);
+    })
+}
 
-var promise = new promise(function(resolve, reject){
-    resolve(1);
-});
-
-promise.then(function(val){
-    console.log(val);
-    return val + 2;
-}).then(function(val){
-    console.log(val);
-})
+function promise3(){
+    function a(){
+        console.log("a run....");
+        return new Promise(function(resolve, reject){
+            setTimeout(function(){
+                resolve(' i am A');
+            }, 2000)
+        })
+    }
+    
+    function b(str){
+        console.log("b run....");
+        setTimeout(function(){
+            console.log("넘겨받은값 : ", str)
+        }, 1000)
+    }
+    a().then(function(str){
+        b(str);
+    })
+}
+promise3();
